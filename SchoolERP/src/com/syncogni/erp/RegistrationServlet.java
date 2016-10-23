0275d4b933b14c5b299fa5dc074ca39b5a453333
Dinesh Bhatt <dineshjavafreelancer@gmail.com>
2016-10-23 21:59:36 +0530

    Removed Comment

--- SchoolERP/src/com/syncogni/erp/RegistrationServlet.java
+++ SchoolERP/src/com/syncogni/erp/RegistrationServlet.java
@@ -39,7 +39,6 @@ public class RegistrationServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter writer = response.getWriter();
 
 		Student student = new Student();
