80856453e23864a76fb6f3ec158d8f0e4eb72bd5
Sagar <sagarmishra8888@gmail.com>
2016-10-27 19:57:43 +0530

    added teachers' variables.

--- SchoolERP/src/com/syncogni/erp/model/Teacher.java
+++ SchoolERP/src/com/syncogni/erp/model/Teacher.java
@@ -3,5 +3,43 @@ package com.syncogni.erp.model;
 public class Teacher {
 
 	private int id;
-	
+	private String Name;
+    private String subject;
+    private String Address;
+    
+    public Teacher(){}
+    public Teacher(int id,String Name,String subject,String Address){
+    	super();
+    	this.id=id;
+    	this.Name=Name;
+    	this.subject=subject;
+    	this.Address=Address;
+  }
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+    
+
 }
